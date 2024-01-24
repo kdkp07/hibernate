eab3990c5ac7206c2f4efe517eb46115be11a638
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-01-24 20:02:56 +0530

    Added new address object

--- Hibernate/src/main/java/in/hibernate/App.java
+++ Hibernate/src/main/java/in/hibernate/App.java
@@ -1,8 +1,7 @@
 package in.hibernate;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Date;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -11,6 +10,7 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
+	@SuppressWarnings("deprecation")
 	public static void main( String[] args ) throws IOException
 	{
 		System.out.println( "Jai shree Ganesh" );
@@ -22,21 +22,39 @@ public class App
 
 		// creating a student
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter Student roll number.....");
-		int id = Integer.parseInt(br.readLine());
-		System.out.println("Enter student name.....");
-		String name  = br.readLine();
-		System.out.println("Enter student city name.....");
-		String city  = br.readLine();
+		//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//		System.out.println("Enter Student roll number.....");
+		//		int id = Integer.parseInt(br.readLine());
+		//		System.out.println("Enter student name.....");
+		//		String name  = br.readLine();
+		//		System.out.println("Enter student city name.....");
+		//		String city  = br.readLine();
+		//
+		//		Student st = new Student(id,name,city);
 
-		Student st = new Student(id,name,city);
+		// creating object of Address class
+		Address add = new Address();
+		add.setAddress_id(23455);
+		add.setCity("Bangalore");
+		add.setStreet("545,Whitefield");
+		add.setDateAdded(new Date());
+		add.setX(34.22);
+		add.setOpen(true);
+
+		// reading an image
+		//		FileInputStream file = new FileInputStream("src/main/java/pic.jpg"); 
+		//		byte[] data = new byte[file.available()];
+		//		file.read(data);
+		//		add.setImage(data);
 
 		Session session = factory.openSession();
-		Transaction tx = session.beginTransaction();// starting the transaction
 
-		session.save(st);
+		Transaction tx = session.beginTransaction();// starting the transaction
+		//		session.save(st);
+		session.save(add);
 		tx.commit();
+
+
 		session.close();
 
 
