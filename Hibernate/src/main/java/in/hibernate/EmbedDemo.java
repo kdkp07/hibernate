2fc42392cf5ed23570d07e2565575e6836d39756
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-01-31 11:08:08 +0530

    Different states of hibernate Objects

--- Hibernate/src/main/java/in/hibernate/EmbedDemo.java
+++ Hibernate/src/main/java/in/hibernate/EmbedDemo.java
@@ -17,19 +17,34 @@ public class EmbedDemo {
 
 		// student ka object jisme certificate embed krege
 		Student st = new Student();
-		st.setId(204);
+		st.setId(205);
 		st.setName("Avinash Jamal");
 		st.setCity("Tamil Nadu");
 
 		Certificate certificate = new Certificate("Java","2 months");
+		// transient state
 		st.setCerti(certificate);
+
 
 		// transaction start krte h
 		Transaction tx = session.beginTransaction();
+
 		session.save(st);
+
+		// persistent state
+		st.setName("John");
+
 		Student student = session.get(Student.class,203);
 		System.out.println(student);
+
+		// removed state
+		session.delete(student);
+
 		tx.commit();
+
+		session.close();
+		// detached state
+		st.setName("Ravi kumar");
 		factory.close();
 
 		System.out.println("==============Done===========");
